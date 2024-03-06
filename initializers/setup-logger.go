629e098c0a7e312607b03962c55c12b4a668cb87
UVMapping <107897863+KendallRey@users.noreply.github.com>
2024-03-06 15:19:06 +0800

    update gin logger directory

--- initializers/setup-logger.go
+++ initializers/setup-logger.go
@@ -11,7 +11,18 @@ import (
 )
 
 func SetupLogger() {
+	logsDir := "logs"
+	err := os.MkdirAll(logsDir, os.ModePerm)
+	if err != nil {
+		panic(fmt.Sprintf("failed to create logs directory: %v", err))
+	}
+
 	today := time.Now().Format("2006-01-02_15-04-05")
-	f, _ := os.Create(fmt.Sprintf("[%s].log", today))
+	logFilePath := fmt.Sprintf("%s/%s.log", logsDir, today)
+	f, err := os.Create(logFilePath)
+	if err != nil {
+		panic(fmt.Sprintf("failed to create log file: %v", err))
+	}
+	
 	gin.DefaultWriter = io.MultiWriter(f, os.Stdout)
 }
